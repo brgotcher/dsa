dd37a9145f83d74bdc09ede7585d0efdedf760d7
EC2 Default User <ec2-user@ip-172-31-92-40.ec2.internal>
2020-01-07 10:19:08 +0000

    attempt to begin main work

--- main.cpp
+++ main.cpp
@@ -2,3 +2,16 @@
 #include "Binary_node.h"
 #include "Binary_tree.h"
 #include "errorcode.h"
+
+int main() {
+    
+    Binary_node<int> rt = Binary_node<int>(1);
+    rt.left = new Binary_node<int>(2);
+    rt.right = new Binary_node<int>(3);
+    
+    Binary_tree<int> tree = Binary_tree<int>();
+    
+    std::cout << "test" << endl;
+    return 0;
+    
+}
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 bst: clean main.o 
 	g++ -o bst main.o 
 main.o: main.cpp Binary_tree.h Search_tree.h errorcode.h
-	g++ -c -g -Wall main.cpp
+	g++ -c -g -Wall -std=c++11 main.cpp
 clean:
 	rm -f *.o bst
